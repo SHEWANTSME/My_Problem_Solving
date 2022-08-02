ac461cc05776b72dc3e3e6b8ff9c6bbb426927bf
Dorinee <qhdmswns@naver.com>
2022-08-02 20:15:30 +0900

    0802 CPP
    오늘의 문제!

---
+++ 0802.cpp
@@ -0,0 +1,21 @@
+// 1937 øÂΩ…¿Ô¿Ã PANDA
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+using namespace std;
+int arr[501][501];
+int n;
+int main() {
+	// Restarting PS
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+
+	
+}
