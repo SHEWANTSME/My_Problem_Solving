7db7dd0c708e0c9aa26da279ef5d7ac0dcf1cac1
SHEWANTSME <qhdmswns@mju.ac.kr>
2022-08-10 13:49:03 +0900

    0806
    2644번

--- 0806.cpp
+++ 0806.cpp
@@ -5,37 +5,30 @@
 using namespace std;
 // 2644 촌수계산
 // have to make tree for sure..
+//그냥 이차원 배열로 하면 될..수도?
 int n, dad, son, m;
-vector<int>v[101];
-void check(int x, int y) {
-
-}
+int arr[101][101];
+int visited[101];
 
 int main() {
 	cin >> n >> dad >> son >> m;
 	for (int i = 0; i < m; i++) {
 		int x, y;	 cin >> x >> y;
-		v[x].push_back(y);
+		arr[x][y] = 1; arr[y][x] = 1;
 	}
-
-	for (int i = 0; i <= 100; i++) {
-		if (v[i].empty())continue;
-		else {
-			for (auto &e : v[i]) {
-				
+	queue<int>q;
+	q.push(dad);
+	while (not q.empty()) {
+		int fr = q.front();
+		q.pop();
+		for (int i = 1; i <= n; i++) {
+			if (arr[fr][i] != 0 and not visited[i]) {
+				q.push(i);
+				visited[i] = visited[fr] + 1;
 			}
 		}
 	}
-	//for (int i = 0; i < 100; i++) {
-	//	if (v[i].empty())continue;
-	//	else {
-	//		for (auto &e : v[i]) {
-	//			cout <<i <<" "<<e << endl;
-	//		}
-	//		//cout << v[i] << endl;
-	//	}
-	//	//cout << v[i].<< endl;
-	//}
-
-
+	if (visited[son] == 0) cout << -1 << endl;
+	else cout << visited[son] << endl;
 }
+
