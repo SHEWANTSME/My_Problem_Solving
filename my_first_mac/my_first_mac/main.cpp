9dc656aaf73ffcd8435c24a1741a5796ca1d68bd
SHEWANTSME <qhdmswns@mju.ac.kr>
2022-12-22 15:30:19 +0900

    new to mac

---
+++ my_first_mac/my_first_mac/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  my_first_mac
+//
+//  Created by 김준현 on 2022/12/22.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"what the fuck"<<"/n";
+    cout<< "hello world"<<endl;
+}
