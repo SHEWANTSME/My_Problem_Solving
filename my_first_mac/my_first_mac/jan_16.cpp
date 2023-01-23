b00948e6cce503f60817e85858fb263438c1e270
SHEWANTSME <qhdmswns@mju.ac.kr>
2023-01-23 18:25:12 +0900

    updated

---
+++ my_first_mac/my_first_mac/jan_16.cpp
@@ -0,0 +1,8 @@
+//
+//  jan_16.cpp
+//  my_first_mac
+//
+//  Created by 김준현 on 2023/01/16.
+//
+
+#include <stdio.h>
---
+++ my_first_mac/my_first_mac/jan_19.cpp
@@ -0,0 +1,8 @@
+//
+//  jan_19.cpp
+//  my_first_mac
+//
+//  Created by 김준현 on 2023/01/19.
+//
+
+#include <stdio.h>
